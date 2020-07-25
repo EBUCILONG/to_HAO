cfb8a4842dc9d457dcff226e7c6b17a00ea0bc93
EBUCILONG <940246862@qq.com>
2020-07-25 14:05:18 +0800

    final

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
-for i in 2000000 4000000 8000000 10000000
+for i in 2000000 5000000 8000000 10000000
 do
     echo Testing $i Continuous Insertion
-    ./build/test $i;
     ./build/test_R $i;
+    ./build/test $i;
 done
 
